5ed3920a593780981667942db467893034113a8c
Ricky <phpgeek@qq.com>
2016-11-04 23:00:50 +0800

    index rindex include?

--- base/string_book_three.rb
+++ base/string_book_three.rb
@@ -37,5 +37,28 @@ puts 'haha'.split(//).inspect #这种匹配会将字符一个一个拆分
 puts 'I am Ricky I love Ruby'.split(/ /, 3).inspect  #第三个参数拆分的数组长度  先按第一个拆分，直到长度为第三个参数-1 剩余部分作为一个元素
 
 
+#删除字符串末尾的换行符
+print "abc\n"
+print "abc\n".chomp#会删除末尾最后一个换行符
+print "abc\n".chop#会删除末尾最后一个字符
+print "abc".chop
+puts ''
 
 
+#查询某个字符串中是否有某个字符
+#index 会从左向右检查，如果有则返回存在的下标      rindex会从右向左检查 返回下标
+str = 'laikanwo aaa'
+
+puts str.index('wo')   #6
+puts str.slice(6, 2)
+
+
+str = 'laikanwo aaa'
+puts str.rindex('wo')
+
+puts str.index('a')
+puts str.index('h').nil?
+
+
+#如果只是想单纯的检查字符串里面是否有某个字符串  可以使用include?
+puts "hahahah".include?('ha')
