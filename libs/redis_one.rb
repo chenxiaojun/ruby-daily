1127a9a903d5cea7e0761182071a2d6bf48144b7
chenxiaojun <chenxiaojun@zaitu.cn>
2016-11-11 14:36:06 +0800

    array merge

---
+++ libs/redis_one.rb
@@ -0,0 +1,9 @@
+require 'redis'
+redis = Redis.new
+p redis ##<Redis client v3.3.1 for redis://127.0.0.1:6379/0>
+
+# redis.set('name', 'micky')
+# redis.set('address', 'beijing', ex:10)
+# p redis.get('name')
+
+
