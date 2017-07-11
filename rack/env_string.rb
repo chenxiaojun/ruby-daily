bb333a99d4f19054d27fa8061d7fe3251484e3a2
Ricky <rickycxj@gmail.com>
2017-07-11 11:59:48 +0800

    rack

---
+++ rack/env_string.rb
@@ -0,0 +1,9 @@
+require 'rack'
+require 'pry'
+
+app = proc do |env|
+  env.to_a.sort_by{ |e| e[0] }.each{ |k, v| puts %Q(#{k} => #{v}) }
+  [200, {}, []]
+end
+
+Rack::Handler::WEBrick.run(app, :Port => 8090)
