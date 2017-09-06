a5b46e5c3a2b37a58692059c0131b8632a5ffcb5
Ricky <rickycxj@gmail.com>
2017-09-06 11:02:45 +0800

    functions

--- array_hash/functions.rb
+++ array_hash/functions.rb
@@ -1,5 +1,5 @@
 # select
 wanted_keys = %w[one two]
 x = { one: 'one', two: 'two', three: 'three' }
-y = x.select { |key, _| wanted_keys.include? key }
+y = x.select { |key, _| p wanted_keys.include? key.to_s }
 puts y
